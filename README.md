b2e3d155470e2e2a3dda1f570a2756e79ee1e6b8
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-09 23:06:30 +0100

    README added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python computer vision - Learning/ playing around repository
+
+## Technologies used for ML here are
+
+* OpenCV
+* numpy
--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 import cv2
+import random
 
-img = cv2.imread("assets/python.png", 0)
-img = cv2.resize(img, (0, 0), fx=5, fy=5)
-img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-cv2.imwrite("assets/updated_python.png", img)
+img = cv2.imread("assets/python.png", 1)
+# img = cv2.resize(img, (0, 0), fx=5, fy=5)
+# img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+# cv2.imwrite("assets/updated_python.png", img)
 
-cv2.imshow("Profile Pic", img)
+# cv2.imshow("Pic", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# print(img[250:400])
+
+# for i in range(100):
+#     for j in range(img.shape[1]):
+#         img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
